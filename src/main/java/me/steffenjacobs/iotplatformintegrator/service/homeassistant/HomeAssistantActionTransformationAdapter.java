7519325142ac3e5382518ea74d5f017a4aa1293a
Steffen Jacobs <sjacobs@mail.uni-mannheim.de>
2019-06-20 16:28:11 +0200

    added support for HomeAssistant's service actions

--- src/main/java/me/steffenjacobs/iotplatformintegrator/service/homeassistant/HomeAssistantActionTransformationAdapter.java
+++ src/main/java/me/steffenjacobs/iotplatformintegrator/service/homeassistant/HomeAssistantActionTransformationAdapter.java
@@ -1,9 +1,13 @@
 package me.steffenjacobs.iotplatformintegrator.service.homeassistant;
 
+import java.util.HashMap;
 import java.util.Map;
 
+import me.steffenjacobs.iotplatformintegrator.domain.shared.item.SharedItem;
+import me.steffenjacobs.iotplatformintegrator.domain.shared.item.ItemType.Command;
 import me.steffenjacobs.iotplatformintegrator.domain.shared.rule.action.ActionType;
 import me.steffenjacobs.iotplatformintegrator.domain.shared.rule.action.SharedAction;
+import me.steffenjacobs.iotplatformintegrator.domain.shared.rule.action.ActionType.ActionTypeSpecificKey;
 import me.steffenjacobs.iotplatformintegrator.service.shared.ItemDirectory;
 
 /** @author Steffen Jacobs */
@@ -16,16 +20,25 @@ public class HomeAssistantActionTransformationAdapter {
 		}
 		@SuppressWarnings("unchecked")
 		Map<String, Object> map = (Map<String, Object>) o;
-		ActionType actionType = parseActionType("" + map.get("action"));
+
+		if (map.containsKey("service")) {
+			String service = "" + map.get("service");
+
+			String itemName = service.substring(0, service.lastIndexOf("."));
+			String command = service.substring(service.lastIndexOf(".")+1, service.length());
+			Command cmd = Command.parse(command);
+
+			SharedItem item = itemDirectory.getItemByName(itemName);
+			String description = String.format("Execute service %s.", item);
+			String label = "Service execution";
+
+			Map<String, Object> properties = new HashMap<>();
+			properties.put(ActionTypeSpecificKey.ItemName.getKeyString(), item);
+			properties.put(ActionTypeSpecificKey.Command.getKeyString(), cmd);
+			return new SharedAction(ActionType.ItemCommand, properties, description, label);
+		}
 
 		return null;
-	}
-
-	private ActionType parseActionType(String actionType) {
-		switch (actionType) {
-		default:
-			return ActionType.Unknown;
-		}
 	}
 
 }
