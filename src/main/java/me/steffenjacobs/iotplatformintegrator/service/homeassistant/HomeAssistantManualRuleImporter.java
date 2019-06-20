4d3cd8a87e7d77ad11bcc3702e115f4eb1a5f8e0
Steffen Jacobs <sjacobs@mail.uni-mannheim.de>
2019-06-20 16:00:28 +0200

    removed unused import

--- src/main/java/me/steffenjacobs/iotplatformintegrator/service/homeassistant/HomeAssistantManualRuleImporter.java
+++ src/main/java/me/steffenjacobs/iotplatformintegrator/service/homeassistant/HomeAssistantManualRuleImporter.java
@@ -1,7 +1,6 @@
 package me.steffenjacobs.iotplatformintegrator.service.homeassistant;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.ArrayList;
