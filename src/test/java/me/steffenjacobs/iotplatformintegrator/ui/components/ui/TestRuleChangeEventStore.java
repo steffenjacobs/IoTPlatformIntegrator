ca602d8eb7a6ae55e07eaf1a16221ff6c3721d81
Steffen Jacobs <steffen.jacobs@hotmail.de>
2019-06-29 17:27:37 +0200

    added test case to modify the description of a trigger via RuleChangeEventStore

---
+++ src/test/java/me/steffenjacobs/iotplatformintegrator/ui/components/ui/TestRuleChangeEventStore.java
@@ -0,0 +1,70 @@
+package me.steffenjacobs.iotplatformintegrator.ui.components.ui;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import me.steffenjacobs.iotplatformintegrator.domain.manage.SharedRuleElementDiff;
+import me.steffenjacobs.iotplatformintegrator.domain.shared.item.ItemType;
+import me.steffenjacobs.iotplatformintegrator.domain.shared.item.SharedItem;
+import me.steffenjacobs.iotplatformintegrator.domain.shared.item.ItemType.Command;
+import me.steffenjacobs.iotplatformintegrator.domain.shared.rule.SharedRule;
+import me.steffenjacobs.iotplatformintegrator.domain.shared.rule.action.SharedAction;
+import me.steffenjacobs.iotplatformintegrator.domain.shared.rule.condition.SharedCondition;
+import me.steffenjacobs.iotplatformintegrator.domain.shared.rule.trigger.SharedTrigger;
+import me.steffenjacobs.iotplatformintegrator.domain.shared.rule.trigger.TriggerType;
+import me.steffenjacobs.iotplatformintegrator.domain.shared.rule.trigger.TriggerType.TriggerTypeSpecificKey;
+import me.steffenjacobs.iotplatformintegrator.service.ui.components.ui.RuleChangeEventStore;
+
+public class TestRuleChangeEventStore {
+
+	@Test
+	public void testDiffApplicationForTrigger() {
+		Set<SharedTrigger> triggers = new HashSet<>();
+		Set<SharedCondition> conditions = new HashSet<>();
+		Set<SharedAction> actions = new HashSet<>();
+
+		Map<String, Object> properties = new HashMap<>();
+		properties.put(TriggerTypeSpecificKey.Command.getKeyString(), Command.On);
+		properties.put(TriggerTypeSpecificKey.ItemName.getKeyString(),
+				new SharedItem("TestSwitch", "TestLabel", ItemType.Switch));
+
+		SharedTrigger triggerToTest = new SharedTrigger(TriggerType.CommandReceived, properties,
+				"ItemStateChangedDescription", "ItemStateChangedLabel", 1);
+		triggers.add(triggerToTest);
+
+		SharedRule rule = new SharedRule("TestRule", "TestRule.id", "Test Description", "false", "ACTIVE", triggers,
+				conditions, actions);
+
+		SharedRuleElementDiff diff = new SharedRuleElementDiff("NewItemStateChangedDescription", null,
+				TriggerType.CommandReceived, null, null, null, false, 1);
+		RuleChangeEventStore store = new RuleChangeEventStore();
+
+		store.applyDiff(rule, diff);
+
+		Assert.assertEquals("Test Description", rule.getDescription());
+		Assert.assertEquals("TestRule", rule.getName());
+		Assert.assertEquals("TestRule.id", rule.getId());
+		Assert.assertEquals("false", rule.getVisible());
+		Assert.assertEquals("ACTIVE", rule.getStatus());
+
+		Assert.assertTrue(rule.getActions().isEmpty());
+		Assert.assertTrue(rule.getConditions().isEmpty());
+		Assert.assertEquals(1, rule.getTriggers().size());
+		SharedTrigger newTrigger = rule.getTriggers().iterator().next();
+		Assert.assertEquals("NewItemStateChangedDescription", newTrigger.getDescription());
+		Assert.assertEquals("ItemStateChangedLabel", newTrigger.getLabel());
+		Assert.assertEquals(1, newTrigger.getRelativeElementId());
+		Assert.assertEquals(TriggerType.CommandReceived, newTrigger.getTriggerTypeContainer().getTriggerType());
+		Assert.assertEquals(2, newTrigger.getTriggerTypeContainer().getTriggerTypeSpecificValues().size());
+		Assert.assertEquals(Command.On, newTrigger.getTriggerTypeContainer().getTriggerTypeSpecificValues()
+				.get(TriggerTypeSpecificKey.Command));
+		Assert.assertEquals(new SharedItem("TestSwitch", "TestLabel", ItemType.Switch), newTrigger
+				.getTriggerTypeContainer().getTriggerTypeSpecificValues().get(TriggerTypeSpecificKey.ItemName));
+	}
+
+}
