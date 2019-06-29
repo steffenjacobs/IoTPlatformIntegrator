2da5ff3dc078157d4501fbb645c9114a37aaafaf
Steffen Jacobs <steffen.jacobs@hotmail.de>
2019-06-29 16:25:28 +0200

    removed deprecation warning in JAva 9

--- src/test/java/me/steffenjacobs/iotplatformintegrator/ui/util/UrlUtilTest.java
+++ src/test/java/me/steffenjacobs/iotplatformintegrator/ui/util/UrlUtilTest.java
@@ -12,13 +12,13 @@ public class UrlUtilTest {
 	public void testSpecificPort() {
 		Pair<String, Integer> urlWithPort = UrlUtil.parseUrlWithPort("http://localhost:1234");
 		Assert.assertEquals("http://localhost", urlWithPort.getLeft());
-		Assert.assertEquals(new Integer(1234), urlWithPort.getRight());
+		Assert.assertEquals(Integer.valueOf(1234), urlWithPort.getRight());
 	}
 
 	@Test
 	public void testDefaultPort() {
 		Pair<String, Integer> urlWithPort = UrlUtil.parseUrlWithPort("http://localhost");
 		Assert.assertEquals("http://localhost", urlWithPort.getLeft());
-		Assert.assertEquals(new Integer(80), urlWithPort.getRight());
+		Assert.assertEquals(Integer.valueOf(80), urlWithPort.getRight());
 	}
 }
