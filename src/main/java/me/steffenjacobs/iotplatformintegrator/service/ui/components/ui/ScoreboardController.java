8ee72ce0dd01e672c6e43838abeb20c567d79846
Steffen Jacobs <sjacobs@mail.uni-mannheim.de>
2019-08-10 13:51:39 +0200

    Improved exception handling with empty database

--- src/main/java/me/steffenjacobs/iotplatformintegrator/service/ui/components/ui/ScoreboardController.java
+++ src/main/java/me/steffenjacobs/iotplatformintegrator/service/ui/components/ui/ScoreboardController.java
@@ -7,12 +7,17 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import me.steffenjacobs.iotplatformintegrator.App;
 import me.steffenjacobs.iotplatformintegrator.domain.authentication.UserScore;
 import me.steffenjacobs.iotplatformintegrator.service.manage.util.SimplifiedSubscriber;
 
 /** @author Steffen Jacobs */
 public class ScoreboardController {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(ScoreboardController.class);
 
 	public ScoreboardController scoreboardController;
 
@@ -37,7 +42,7 @@ public class ScoreboardController {
 		try {
 			return future.get(10, TimeUnit.SECONDS);
 		} catch (InterruptedException | ExecutionException | TimeoutException e) {
-			e.printStackTrace();
+			LOG.warn("Database is empty or not available.");
 			return new UserScore[0];
 		}
 	}
