239aab443215e7146ed4da40c9d532a15ec14abe
Steffen Jacobs <sjacobs@mail.uni-mannheim.de>
2019-08-15 13:31:52 +0200

    improved creation of new rules + added working defaults

--- src/main/java/me/steffenjacobs/iotplatformintegrator/service/ui/components/ui/RuleMutator.java
+++ src/main/java/me/steffenjacobs/iotplatformintegrator/service/ui/components/ui/RuleMutator.java
@@ -6,21 +6,27 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 
+import me.steffenjacobs.iotplatformintegrator.domain.shared.item.ItemType.Command;
+import me.steffenjacobs.iotplatformintegrator.domain.shared.item.ItemType.Operation;
 import me.steffenjacobs.iotplatformintegrator.domain.shared.rule.SharedElementType;
 import me.steffenjacobs.iotplatformintegrator.domain.shared.rule.SharedRule;
 import me.steffenjacobs.iotplatformintegrator.domain.shared.rule.SharedRuleElement;
+import me.steffenjacobs.iotplatformintegrator.domain.shared.rule.action.ActionType;
+import me.steffenjacobs.iotplatformintegrator.domain.shared.rule.action.ActionType.ActionTypeSpecificKey;
 import me.steffenjacobs.iotplatformintegrator.domain.shared.rule.action.SharedAction;
 import me.steffenjacobs.iotplatformintegrator.domain.shared.rule.condition.ConditionType;
-import me.steffenjacobs.iotplatformintegrator.domain.shared.rule.action.ActionType;
+import me.steffenjacobs.iotplatformintegrator.domain.shared.rule.condition.ConditionType.ConditionTypeSpecificKey;
 import me.steffenjacobs.iotplatformintegrator.domain.shared.rule.condition.SharedCondition;
 import me.steffenjacobs.iotplatformintegrator.domain.shared.rule.trigger.SharedTrigger;
 import me.steffenjacobs.iotplatformintegrator.domain.shared.rule.trigger.TriggerType;
+import me.steffenjacobs.iotplatformintegrator.domain.shared.rule.trigger.TriggerType.TriggerTypeSpecificKey;
 import me.steffenjacobs.iotplatformintegrator.service.manage.EventBus;
 import me.steffenjacobs.iotplatformintegrator.service.manage.EventBus.EventType;
 import me.steffenjacobs.iotplatformintegrator.service.manage.events.RuleChangeEvent;
 import me.steffenjacobs.iotplatformintegrator.service.manage.events.RuleElementCopiedEvent;
 import me.steffenjacobs.iotplatformintegrator.service.manage.events.RuleElementCreatedEvent;
 import me.steffenjacobs.iotplatformintegrator.service.manage.events.RuleElementRemovedEvent;
+import me.steffenjacobs.iotplatformintegrator.service.shared.ItemDirectory;
 import me.steffenjacobs.iotplatformintegrator.ui.components.rulebuilder.DynamicElement.ElementType;
 
 /** @author Steffen Jacobs */
@@ -41,28 +47,66 @@ public class RuleMutator {
 		oRule.ifPresent(selectedRule -> {
 			switch (elementType) {
 			case TRIGGER:
-				final Map<String, Object> properties = new HashMap<String, Object>();
 				int relativeElementId = findHighestId(selectedRule.getTriggers()) + 1;
-				final SharedTrigger trigger = new SharedTrigger((TriggerType) sharedElementType, properties, "TRIGGER-" + relativeElementId, "-", relativeElementId);
+				final SharedTrigger trigger = new SharedTrigger((TriggerType) sharedElementType, createDefaults(sharedElementType), "TRIGGER-" + relativeElementId, "-",
+						relativeElementId);
 				selectedRule.getTriggers().add(trigger);
 				EventBus.getInstance().fireEvent(new RuleChangeEvent(selectedRule, trigger, null));
 				break;
 			case CONDITION:
-				final Map<String, Object> properties2 = new HashMap<String, Object>();
 				int relativeElementId2 = findHighestId(selectedRule.getConditions()) + 1;
-				final SharedCondition condition = new SharedCondition((ConditionType) sharedElementType, properties2, "CONDITION-" + relativeElementId2, "-", relativeElementId2);
+				final SharedCondition condition = new SharedCondition((ConditionType) sharedElementType, createDefaults(sharedElementType), "CONDITION-" + relativeElementId2, "-",
+						relativeElementId2);
 				selectedRule.getConditions().add(condition);
 				EventBus.getInstance().fireEvent(new RuleChangeEvent(selectedRule, condition, null));
 				break;
 			case ACTION:
-				final Map<String, Object> properties3 = new HashMap<String, Object>();
 				int relativeElementId3 = findHighestId(selectedRule.getActions()) + 1;
-				final SharedAction action = new SharedAction((ActionType) sharedElementType, properties3, "ACTION-" + relativeElementId3, "-", relativeElementId3);
+				final SharedAction action = new SharedAction((ActionType) sharedElementType, createDefaults(sharedElementType), "ACTION-" + relativeElementId3, "-",
+						relativeElementId3);
 				selectedRule.getActions().add(action);
 				EventBus.getInstance().fireEvent(new RuleChangeEvent(selectedRule, action, null));
 				break;
 			}
 		});
+	}
+
+	private Map<String, Object> createDefaults(SharedElementType type) {
+		final Map<String, Object> map = new HashMap<>();
+		if (type instanceof TriggerType) {
+			for (TriggerTypeSpecificKey key : ((TriggerType) type).getTypeSpecificKeys()) {
+				map.put(key.getKeyString(), "");
+			}
+		} else if (type instanceof ConditionType) {
+			for (ConditionTypeSpecificKey key : ((ConditionType) type).getTypeSpecificKeys()) {
+				map.put(key.getKeyString(), "");
+			}
+		} else if (type instanceof ActionType) {
+			for (ActionTypeSpecificKey key : ((ActionType) type).getTypeSpecificKeys()) {
+				map.put(key.getKeyString(), "");
+			}
+		}
+
+		final ItemDirectory dummyCreator = new ItemDirectory();
+		map.computeIfPresent(TriggerTypeSpecificKey.Command.getKeyString(), (k, v) -> Command.Off);
+		map.computeIfPresent(TriggerTypeSpecificKey.PreviousState.getKeyString(), (k, v) -> 0);
+		map.computeIfPresent(TriggerTypeSpecificKey.State.getKeyString(), (k, v) -> 0);
+		map.computeIfPresent(TriggerTypeSpecificKey.Channel.getKeyString(), (k, v) -> "channel-0");
+		map.computeIfPresent(TriggerTypeSpecificKey.Event.getKeyString(), (k, v) -> "");
+		map.computeIfPresent(TriggerTypeSpecificKey.EventData.getKeyString(), (k, v) -> "");
+		map.computeIfPresent(TriggerTypeSpecificKey.ItemName.getKeyString(), (k, v) -> dummyCreator.getItemByName(null));
+		map.computeIfPresent(TriggerTypeSpecificKey.Time.getKeyString(), (k, v) -> "");
+		map.computeIfPresent(ConditionTypeSpecificKey.Script.getKeyString(), (k, v) -> "");
+		map.computeIfPresent(ConditionTypeSpecificKey.Type.getKeyString(), (k, v) -> "");
+		map.computeIfPresent(ConditionTypeSpecificKey.Operator.getKeyString(), (k, v) -> Operation.BIGGER_EQUAL);
+		map.computeIfPresent(ConditionTypeSpecificKey.StartTime.getKeyString(), (k, v) -> "");
+		map.computeIfPresent(ConditionTypeSpecificKey.EndTime.getKeyString(), (k, v) -> "");
+		map.computeIfPresent(ActionTypeSpecificKey.Enable.getKeyString(), (k, v) -> false);
+		map.computeIfPresent(ActionTypeSpecificKey.RuleUUIDs.getKeyString(), (k, v) -> "");
+		map.computeIfPresent(ActionTypeSpecificKey.Sink.getKeyString(), (k, v) -> "");
+		map.computeIfPresent(ActionTypeSpecificKey.Sound.getKeyString(), (k, v) -> "");
+		map.computeIfPresent(ActionTypeSpecificKey.ConsiderConditions.getKeyString(), (k, v) -> false);
+		return map;
 	}
 
 	private int findHighestId(Set<? extends SharedRuleElement> set) {
